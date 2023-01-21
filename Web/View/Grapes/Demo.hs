5580018666b8e76aa75b0ed084e456a52d0fef46
Alex Greif <alex.greif@gmail.com>
2023-01-21 14:55:58 +0100

    grapesjs local storageManager

--- Web/View/Grapes/Demo.hs
+++ Web/View/Grapes/Demo.hs
@@ -1256,7 +1256,7 @@ instance View DemoView where
         },
 
         storageManager: {
-            type: 'remote', // Storage type. Available: local | remote
+            type: 'local', // Storage type. Available: local | remote
             autosave: true, // Store data automatically
             autoload: true, // Autoload stored data on init
             stepsBeforeSave: 1, // If autosave is enabled, indicates how many changes are necessary before the store method is triggered
